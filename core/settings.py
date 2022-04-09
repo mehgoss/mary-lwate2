d5f1197231c1fe84ddc8a0e439607b9b4b8a88f6
mehgoss <60152334+mehgoss@users.noreply.github.com>
2022-04-09 13:26:45 +0200

    fixing settings

--- core/settings.py
+++ core/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'media')]
 
@@ -140,6 +140,6 @@ EMAIL_PORT = 587
 
 # Optional SMTP authentication information for EMAIL_HOST.
 EMAIL_HOST_USER = 'miguelmehgoss@gmail.com'
-EMAIL_HOST_PASSWORD = 'Waserd4343'
+
 EMAIL_USE_TLS = False
 EMAIL_USE_TLS = True
