f5748380b3670b493abd2d83f9b56d1f5f36f639
mehgoss <miguelmehgoss@gmail.com>
2022-04-10 11:27:02 +0200

    hostfix

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p46@$l*96e&cm=oo_=3z2^qwao*tjsvgw%8c$-(m4ed5h(+l*n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['marylwate.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['marylwate.herokuapp.com', '127.0.0.1', '.herokuap.com']
 
 
 # Application definition
